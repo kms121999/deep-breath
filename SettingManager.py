ebfd78589d769360b3bd8ec11b7db4d5be732710
Keaton Smith <keaton.smith365@gmail.com>
2024-10-19 05:52:31 -0600

    updated setting manager - thread safe

--- SettingManager.py
+++ SettingManager.py
@@ -1,4 +1,5 @@
 import pickle
+import threading
 
 DEFAULT_SETTINGS = {
     "processSettings": {
@@ -11,18 +12,39 @@ DEFAULT_SETTINGS = {
 
 }
 
+SETTINGS_FILE = 'data/user_settings.pkl'
+
 class SettingManager:
     def __init__(self):
+        # If you take both locks, you must take disk_lock first and then the other lock
+        self.lock = threading.Lock()
+
+        # Allows the in-memory lock to be released while writing to disk
+        # This prevents tracker from being blocked while writing to disk
+        self.disk_lock = threading.Lock()
+
         self.settings = {}
-        try:
-            with open('user_settings.pkl', 'rb') as file:
-                self.settings = pickle.load(file)
-        except (FileNotFoundError):
-            self.settings = dict(DEFAULT_SETTINGS)
-            
+        with self.disk_lock:
+            # Since settings is not populated, take the lock to prevent access to empty settings
+            with self.lock:
+                try:
+                    with open(SETTINGS_FILE, 'rb') as file:
+                        self.settings = pickle.load(file)
+                except (FileNotFoundError):
+                    self.settings = dict(DEFAULT_SETTINGS)
+    
+    def get_settings(self):
+        with self.lock:
+            return self.settings
+    
+    def set_settings(self, settings):
+        with self.disk_lock:
+            with self.lock:
+                self.settings = settings
+
+            with open(SETTINGS_FILE, 'wb') as file:
+                pickle.dump(settings, file)
 
     def get(self, key):
-        return self.settings.get(key)
-
-    def set(self, key, value):
-        self.settings[key] = value
+        with self.lock:
+            return self.settings.get(key)
